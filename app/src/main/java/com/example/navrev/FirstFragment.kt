80abbbabe0742da94eebd00a6a1d326c9836e889
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-10-25 22:27:32 +0530

    navigate action creatded in navgraph and navigatiomn executed

--- app/src/main/java/com/example/navrev/FirstFragment.kt
+++ app/src/main/java/com/example/navrev/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,13 @@ class FirstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val s= Navigation.createNavigateOnClickListener(R.id.action_firstFragment_to_secondFragment)
+        val button:Button= view.findViewById(R.id.button_frag1)
+        button.setOnClickListener(s)
     }
 
     companion object {
